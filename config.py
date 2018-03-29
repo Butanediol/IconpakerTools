c6de83619764df174109db7198e98aafb5a21ae3
Liu Xuan <n872485238@gmail.com>
2018-03-30 00:41:22 +0800

    增加配置文件

---
+++ config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# 这个文件是配置文件
+
+# 手动搜索Appfilter并保存到本地(会覆盖下面的设置，若开启，下面全部内容失效，未开工)
+mannualAppfilterMode = False
+
+# 是否输入Iconpack.xml所需内容
+needIconpackXml = True
+
+# 是是否生成Drawable.xml所需内容
+needDrawableXml = True
+
+# 是否联网获取Appfilter.xml
+needAppfilterXml = False
+
+# 是否生成主题图标文件（未开工）
+needXUIIconFile = False
