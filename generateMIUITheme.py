0ac5307a30f22fd5909bc2f5cd8ff1139f5dcf97
Butanediol <n872485238@gmail.com>
2018-10-18 00:18:34 +0800

    MIUI icon generator

---
+++ generateMIUITheme.py
@@ -0,0 +1,33 @@
+import xml.etree.ElementTree as XET
+import os
+
+def analyseAppfilter():
+	appfilterFileLocation = str(path)+'/appfilter.xml'
+	xmlTree = XET.parse(appfilterFileLocation)
+	xmlRoot = xmlTree.getroot()
+	childAttribution = ''
+	childAttributionPackageName = ''
+	childAttributionLaunchActivity = ''
+	childAttributionIconName = ''
+	print('\nxmlRoot.tag:',xmlRoot.tag,'\nxmlRoot.attrib:',xmlRoot.attrib,'\nxmlRoot.text:',xmlRoot.text)
+
+	counts = 0
+
+	for child in xmlRoot:
+		if child.tag == 'item':
+			childAttribution = str(child.attrib)
+			# print(childAttribution)
+			childAttributionPackageName = childAttribution[29:childAttribution.index('/')] # 获取包名
+			childAttributionLaunchActivity = childAttribution[childAttribution.index('/')+1:childAttribution.index('}')] #获取启动项
+			childAttributionIconName = childAttribution[childAttribution.index('}')+17:childAttribution.index("'}")] # 获取图标名
+
+			if(childAttributionIconName):
+				cmd = 'cp ' + path + '/' + childAttributionIconName + '.png ' + path + '/output/' + childAttributionPackageName + '.png'
+				# print(childAttributionIconName)
+				os.system(cmd)
+				counts += 1
+	print('\n' + str(counts) + ' in total.')
+
+path = input('Please input the dir path.\n')
+os.system('mkdir ' + path + '/output')
+analyseAppfilter()
